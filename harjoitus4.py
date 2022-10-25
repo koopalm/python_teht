885950801c079aa39541d2536e18a628bae929ac
Katri <katri.palmroth@brightstraining.com>
2022-10-25 16:47:24 +0300

    sanalista tekstiedostoon

---
+++ harjoitus4.py
@@ -0,0 +1,18 @@
+with open('harj4.txt','r') as tiedosto:
+    sanalista = []
+    try:
+        for sana in tiedosto:
+            sanalista.append(sana)
+    except NameError:
+        print("Something is not defined")   
+    #print(sanalista)
+sanalista.sort()
+sanalista.sort(key=len)
+print(sanalista)
+
+sanajono = ""
+for sana in sanalista:
+    sanajono += sana
+
+with open("uusitiedosto.txt", "a") as uusi_tiedosto:
+    uusi_tiedosto.write(sanajono)
